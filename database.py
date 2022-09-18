a99b91389cbfae6c7b4ec128301dfdffbfa75d72
Kingsley Ihemere <dekings44@gmail.com>
2022-09-18 23:18:09 +0100

    fastapi project first commit

--- database.py
+++ database.py
@@ -3,13 +3,13 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 import psycopg2
 import os
 
-password = os.environ['PASSWORD']
-user = os.environ['USERNAME1']
-database = 'pizza_delivery'
-hostname = 'localhost'
-port_id = 5432
+# password = os.environ['PASSWORD']
+# user = os.environ['USERNAME1']
+# database = 'pizza_delivery'
+# hostname = 'localhost'
+# port_id = 5432
 
-engine = create_engine('postgresql://user:password@localhost/pizza_delivery',
+engine = create_engine("postgresql://postgres:Kingsley%4005013@localhost/pizza_delivery",
     echo = True
 )
 
