a22c33937b7422e8bcf32fd5817809c4b4b4f102
Kingsley Ihemere <dekings44@gmail.com>
2022-10-29 00:28:50 +0100

    containrise the app

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from auth_routes import auth_router
 from order_routes import order_router
 from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
@@ -16,3 +17,6 @@ app = FastAPI()
 
 app.include_router(auth_router)
 app.include_router(order_router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host="0.0.0.0")
